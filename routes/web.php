46216eb8f96c220852f9e9884749c21f48d24785
Kyohei7 <rizkimuhammad2301@gmail.com>
2020-12-10 14:02:22 +0700

    Add library Mailtrap & Sentry

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/debug-sentry', function () {
-    throw new Exception('My first Sentry error!');
-});
+// Route::get('/debug-sentry', function () {
+//     throw new Exception('My first Sentry error!');
+// });
