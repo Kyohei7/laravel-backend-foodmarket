abdcbd28a688f1dd79ae8d7aea0311b15f3c3daf
Kyohei7 <rizkimuhammad2301@gmail.com>
2020-12-13 11:08:17 +0700

    Create CMS Frondend Delete User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -89,8 +89,11 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        // Delete User
+        $user->delete();
+
+        return redirect()->route('users.index');
     }
 }
